5f0baae8358f52260f723499a19ed4920d9ad332
Royal-lobster <srujangs8@gmail.com>
2021-11-18 20:10:49 +0530

    final build

--- app.js
+++ app.js
@@ -1,63 +1,65 @@
-const modal = document.querySelector('.modal')
-const modalPara = document.querySelector('.modal__para')
-const modalHead = document.querySelector('.modal__heading')
-const modalButtons = document.querySelectorAll('.modal__button')
-const modalCloseButton = document.querySelector('.modal__closeBtn')
-const darkModeBtn = document.querySelector('.darkModeBtn')
-const speakBtn = document.querySelector(".modal__speakBtn")
-let fishSong = `One, two, three, four, five, Once I caught a fish alive, Six, seven, eight, nine, ten, Then I let go again. Why
-did you let it go? Because it bit my finger so. Which finger did it bite? This little finger on the right`
+async function main() {
+    const modal = document.querySelector('.modal')
+    const modalPara = document.querySelector('.modal__para')
+    const modalHead = document.querySelector('.modal__heading')
+    const modalCloseButton = document.querySelector('.modal__closeBtn')
+    const darkModeBtn = document.querySelector('.darkModeBtn')
+    const speakBtn = document.querySelector(".modal__speakBtn")
+    const buttonContainer = document.querySelector('.buttonsContainer')
 
-let abcSong = `A B C D E F G
-H I J K L M N O P
-Q R S T U V
-W X Y and Z
+    // fetching rhymes data from data.json 
+    const response = await fetch('./data.json');
+    const data = await response.json()
+    const rhymesData = data.data;
 
-Now I know my ABCs
-Next time won’t you sing with me`
+    // add buttons to html based on data in rhyme data 
+    rhymesData.forEach((rhymeData, id) => {
+        // create a new button
+        let addedButton = document.createElement('button');
 
-let sheepSong = `Baa, baa black sheep
-Have you any wool
-Yes sir, yes sir
-Three bags full.
+        // give properties to that button
+        addedButton.id = `modalButton--${id}`
+        addedButton.className = 'modal__button';
+        addedButton.textContent = rhymeData.name;
 
-One for my master
-And one for my dame
-And one for the little boy
-Who lives down the lane.
-`
-modalButtons.forEach(btn => {
-    btn.addEventListener('click', function () {
-        if (btn.textContent == "Fish song") {
-            modalPara.textContent = fishSong;
-            modalHead.textContent = "Fish Song"
-            speakBtn.addEventListener("click", function () {
-                responsiveVoice.speak(fishSong);
-            })
-        } else if (btn.textContent == "ABC song") {
-            modalPara.textContent = abcSong;
-            modalHead.textContent = "Alphabet Song"
-            speakBtn.addEventListener("click", function () {
-                responsiveVoice.speak(abcSong);
-            })
-        } else if (btn.textContent == "Sheep song") {
-            modalPara.textContent = sheepSong;
-            modalHead.textContent = "Baa Baa Black Sheep"
-            speakBtn.addEventListener("click", function () {
-                responsiveVoice.speak(sheepSong);
-            })
-        }
-        modal.classList.remove('modal--hidden')
+        // place the created button inside button__container
+        buttonContainer.appendChild(addedButton)
+
+        // add an event listener to our appended new button
+        addedButton.addEventListener('click', function () {
+            modalPara.textContent = rhymeData.song
+            modalHead.textContent = rhymeData.name
+            modal.classList.remove('modal--hidden')
+        })
     })
-})
 
-modalCloseButton.addEventListener('click', function () {
-    modal.classList.add('modal--hidden')
-    responsiveVoice.cancel();
-})
-darkModeBtn.addEventListener('click', function () {
-    document.querySelector('html').classList.toggle('dark-mode')
-})
-speakBtn.addEventListener('click', function () {
+    // when close button is pressed hide modal, stop TTS if playing and rename speakBtn 
+    modalCloseButton.addEventListener('click', function () {
+        modal.classList.add('modal--hidden')
+        responsiveVoice.cancel();
+        speakBtn.textContent = "speak";
+    })
 
-})
+    // when darkMode Btn is pressed give dark-mode styles to html
+    darkModeBtn.addEventListener('click', function () {
+        document.querySelector('html').classList.toggle('dark-mode')
+    })
+
+    // when speak button clicked, if playing stop playing else speak
+    speakBtn.addEventListener("click", function () {
+        if (responsiveVoice.isPlaying()) {
+            responsiveVoice.cancel();
+            speakBtn.textContent = "speak";
+        } else {
+            responsiveVoice.speak(modalPara.textContent, "UK English Female", {
+                onend: () => {
+                    speakBtn.textContent = "speak";
+                }
+            })
+            speakBtn.textContent = "🔊 playing";
+        }
+    })
+}
+
+// run main function
+main()
